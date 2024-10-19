858e492d49539fc3e02f06a2a44b874063ec6514
mshirazkamran <blogger31625@gmail.com>
2024-10-19 18:35:03 +0500

    added base files

---
+++ src/main/java/dev/shiraz/GCI/GciApplication.java
@@ -0,0 +1,13 @@
+package dev.shiraz.GCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/shiraz/GCI/GciApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shiraz.GCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
